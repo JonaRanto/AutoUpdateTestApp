308b77cbf53a8eef45e8615a070e9fd199c7341a
Jonathan Navarro Vega <jonathan@ranto.cl>
2021-09-30 11:54:49 -0300

    Se añadió verficación de versión

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import requests
+
 # Verificar versión
+def verificar_version():
+    url_last_readme = 'https://github.com/JonaRanto/AutoUpdateTestApp/raw/main/README.md'
+    page = requests.get(url_last_readme)
+    last_version = page.text.split('\n')[1].split('=')[1]
+    my_readme = open('README.md', 'r')
+    my_version = my_readme.read().split('\n')[1].split('=')[1]
+    resp = False
+    if my_version == last_version: resp = True
+    return resp
+if not verificar_version():
+    print('La aplicación está desactualziada :C')
+else:
+    print('La aplicación está actualziada ^^')
